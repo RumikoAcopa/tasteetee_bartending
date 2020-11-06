11356981d2c49915651d58e1aca376204a6b78ec
rumikoacopa <rumikorios@gmail.com>
2020-11-05 20:36:27 -0500

    add authentication to sign in

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,6 @@
 class DrinksController < ApplicationController
+
+    def index
+         
+    end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_055009) do
+ActiveRecord::Schema.define(version: 2020_11_05_220556) do
+
+  create_table "drinks", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
