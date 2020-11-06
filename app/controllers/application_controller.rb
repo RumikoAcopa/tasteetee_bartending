789f30b649d9c3b9b59f3024e3de7c54ab1d9d8d
rumikoacopa <rumikorios@gmail.com>
2020-11-06 18:40:02 -0500

    added Admin Column to Users and created auth_admin? Method in app controller with a before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_admin!, only: [:action]
 
     protected
     
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
         devise_parameter_sanitizer.permit(:sign_up, keys: [:date_of_birth])
+    end
 
+    def authenticate_admin? 
+        authenticate_user!false
+        redirect_to :login, status::forbidden
+        unless current_user.admin?
     end
 end
