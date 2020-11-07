9d128ba6d9b3d6fd130a0ffa2d7a7e61f3b9d31d
rumikoacopa <rumikorios@gmail.com>
2020-11-06 23:44:54 -0500

    added commented out devise for possible admin in case I decide I need it

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :orders
   resources :drinks
   devise_for :users
+  #devise_for :admin ?
+
   root 'drinks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
