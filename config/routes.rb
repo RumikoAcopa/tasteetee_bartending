356346a032a664a91c25330b2d140c88970ad4e9
rumikoacopa <rumikorios@gmail.com>
2020-11-09 19:23:37 -0500

    added only to resources, commented out devise, added namespace/resources for admin  and changed the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  resources :ingredients
+  #devise_for :admins
   devise_for :users
-  resources :orders
-  resources :drinks
+  resources :orders, only: [:index, :show, :edit, :update, :delete]
+  resources :drinks, only: [:index,:show]
 
-  root 'drinks#index'
+  namespace :admin do
+
+    get '/', to: 'admin#index'
+    get '/show', to: 'admin#show'
+    resources :users, param: :slug, only: [:edit, :update, :destroy]
+    resources :drinks, only: [:new, :create, :edit, :update, :destroy]
+    resources :orders,  only: [:new, :create, :edit, :update, :destroy]
+  end
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
