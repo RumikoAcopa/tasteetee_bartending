37aa0339071206af4e98f14f2e151f606b808bc4
rumikoacopa <rumikorios@gmail.com>
2020-11-09 19:17:22 -0500

    changed associations

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,8 +1,14 @@
 class Drink < ApplicationRecord
-    has_many :orders
+    belongs_to :orders
     has_many :users, through: :orders
   
     validates :name, :ingredients, :price, presence: true
     
     
 end
+
+
+#user has many orders. orders have many drinks. drinks have many ingredients
+#many-to-many relationship implemented with has_many :through associations.
+
+#orders has_many ingredients through:drinks
