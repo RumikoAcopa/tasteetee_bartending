69abe75115466c7befac49d1b1a4ebe75fe21ea2
rumikoacopa <rumikorios@gmail.com>
2020-11-09 19:16:16 -0500

    added devise to admin

--- app/models/admin.rb
+++
@@ -1,8 +0,0 @@
-class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :timeoutable,
-         :recoverable, :rememberable, :validatable
-         #Methods to use for Admin: admin_signed_in?, current_admin, admin_session
-               
-end
