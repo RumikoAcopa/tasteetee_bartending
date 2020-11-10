417cc65338b99b428e3ed59112864a03ff48f44c
rumikoacopa <rumikorios@gmail.com>
2020-11-09 19:27:07 -0500

    schema version changed?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_233249) do
+ActiveRecord::Schema.define(version: 2020_11_09_231151) do
 
   create_table "drinks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_233249) do
     t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -40,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020_11_06_233249) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
